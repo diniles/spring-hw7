5e6a69b2d02a09815fa096f3c76d58e1e390a4c9
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-12 16:13:12 +0300

    Feat: class SecurityConfiguration

---
+++ src/main/java/ru/gb/hw7/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package ru.gb.hw7;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@AllArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(req -> req
+                        .requestMatchers("/register").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("login").permitAll())
+                .logout(logout -> logout.permitAll());
+        return httpSecurity.build();
+    }
+}
