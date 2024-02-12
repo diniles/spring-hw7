3da6552103ff153b0260cf08d5af7da9b2f0700e
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-12 14:02:32 +0300

    Feat: class MainController

---
+++ src/main/java/ru/gb/hw7/MainController.java
@@ -0,0 +1,13 @@
+package ru.gb.hw7;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
