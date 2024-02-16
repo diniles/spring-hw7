6d944c28b35495e29d40109b3853da9269f846e1
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-16 13:47:19 +0300

    Feat: UserDetailsImpl

---
+++ src/main/java/ru/gb/hw7/UserDetailsImpl.java
@@ -0,0 +1,73 @@
+package ru.gb.hw7;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDetailsImpl implements UserDetails {
+    private Long id;
+    private String name;
+    private String password;
+    private Collection<? extends GrantedAuthority> role;
+
+    public UserDetailsImpl(Long id, String name, String password, Collection<? extends GrantedAuthority> role) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> rolesList = List.of(new SimpleGrantedAuthority(User.getRole().name));
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getName(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
