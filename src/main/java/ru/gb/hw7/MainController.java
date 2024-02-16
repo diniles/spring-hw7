c41d92f9712fbe38a594f179d9633b15fcfe90f0
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-16 13:35:38 +0300

    Feat: MainController

--- src/main/java/ru/gb/hw7/MainController.java
+++ src/main/java/ru/gb/hw7/MainController.java
@@ -2,13 +2,17 @@ package ru.gb.hw7;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    UserService userService;
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String main(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "main";
     }
 
     @GetMapping("/login")
